12d8334e3f88218a15706ee7a2808e2d10577f14
DJosey <djosey@users.noreply.github.com>
2018-05-24 00:22:21 -0600

    batch parse update
    much progress on the batch parse. need to modify the operators which were in most cases outputting blank data due to the randomness of the location of the objects within each line's json structure. (searching for "','" string literal would pull the first one on the line, regardless of whether it was before or after the object name string literal)

--- batchparse.py
+++ batchparse.py
@@ -1,7 +1,17 @@
 import os
-directory = "/home/postfreedom/Documents/Reddit_Data/Data/jan16"
-for filename in os.listdir(directory):
-    if filename.startswith("x"):
-        print(os.path.join(directory, filename))
-    else:
-        print 'done'
+directory = "/home/postfreedom/Documents/Reddit_Data/Data/jan16/"
+moving = open('target.txt', 'w')
+for y in os.listdir(directory):
+    if y.startswith("x"):
+        with open(y, "r") as currentFile:
+            for x in currentFile:
+                a = x[(x.index('parent_id')+9):(x.index('","'))]
+                g = x[(x.index('"id"')+4):(x.index('","'))]
+                z = x[(x.index('body')+4):(x.index('","'))]
+                # # moving.write(z + "\n" + a +"\n")
+                # # # y.close()
+                moving.write(z +"\n" + a + "\n" + g)
+
+            else:
+                # moving.close()
+                print 'failure'
