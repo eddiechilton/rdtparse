62ad2d5feb5c41d32966045ea4918a2d1e948929
DJosey <djosey@users.noreply.github.com>
2018-05-24 23:12:11 -0600

    Update batchparse.py

--- batchparse.py
+++ batchparse.py
@@ -1,17 +1,27 @@
 import os
 directory = "/home/postfreedom/Documents/Reddit_Data/Data/jan16/"
 moving = open('target.txt', 'w')
+preStringZ = '"id"'
+searchStringZ = '","'
+preStringA = 'parent_id'
+searchStringA = '","'
+preStringG = 'body'
+searchStringG ='","'
 for y in os.listdir(directory):
     if y.startswith("x"):
-        with open(y, "r") as currentFile:
+        with open(y, "r") as cuhttps://news.google.com/?hl=en-US&gl=US&ceid=US:enrrentFile:
             for x in currentFile:
-                a = x[(x.index('parent_id')+9):(x.index('","'))]
-                g = x[(x.index('"id"')+4):(x.index('","'))]
-                z = x[(x.index('body')+4):(x.index('","'))]
-                # # moving.write(z + "\n" + a +"\n")
-                # # # y.close()
-                moving.write(z +"\n" + a + "\n" + g)
+                z = x[(x.index(preStringZ)+6):(x.index(searchStringZ, x.index(preStringZ)))]
+                a = x[(x.index(preStringA)+15):(x.index(searchStringA, x.index(preStringA)))]
+                g = x[(x.index(preStringG)+7):(x.index(searchStringG, x.index(preStringG)))]
+                # moving.write(z + "\n" + a +"\n")
+                # # y.close()
+                moving.write(z +"\n" + a + "\n" + g + "\n")
+                print z
+                print a
+                print g
+                print y
+            # else:
+            #     # moving.close()
+            #     print 'failure'
 
-            else:
-                # moving.close()
-                print 'failure'
