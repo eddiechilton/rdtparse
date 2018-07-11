9e2885ea004abf54bdf400abfac5d84794b62f77
DJosey <djosey@users.noreply.github.com>
2018-07-11 06:11:01 -0600

    comments

--- bufferparse.py
+++ bufferparse.py
@@ -1,8 +1,9 @@
 import os
-directory = ""
+directory = "/home/postfreedom/Documents/Reddit_Data/jsonparser/"
 moving = open('target.txt', 'w')
 with open("testdata.json", "r") as currentFile:
         for x in currentFile:
+            ##Assign search substrings
             try:
                 preStringZ = '"id"'
                 searchStringZ = '","'
@@ -11,6 +12,7 @@ with open("testdata.json", "r") as currentFile:
                 preStringG = 'body'
                 searchStringG ='","'
                 linebreak = '\n'
+                ##Calculate locations of search substrings for comparison
                 preZ = x.find(preStringZ)
                 preA = x.find(preStringA)
                 preG = x.find(preStringG)
@@ -18,24 +20,32 @@ with open("testdata.json", "r") as currentFile:
                 sreA = x.find(searchStringA, preA + 1)
                 sreG = x.find(searchStringG, preG + 1)
                 lbki = x.find(linebreak)
-                print(preZ)
-                print(preA)
-                print(preG)
-                print(sreZ)
-                print(sreA)
-                print(sreG)
-                print(lbki)
+                ##Disabled Console logs for testing
+                # print(preZ)
+                # print(preA)
+                # print(preG)
+                # print(sreZ)
+                # print(sreA)
+                # print(sreG)
+                # print(lbki)
+                ##Check substring order, reassign searchString if line break comes before the next comma
                 if lbki < sreZ:
                     searchStringZ = linebreak
                     if lbki < sreA:
                         searchStringA = linebreak
                         if lbki < sreG:
                             searchStringG = linebreak
+                ##Writes substrings to objects
                 z = x[(x.find(preStringZ)+6):(x.find(searchStringZ, x.find(preStringZ)))]
                 a = x[(x.find(preStringA)+15):(x.find(searchStringA, x.find(preStringA)))]
                 g = x[(x.find(preStringG)+7):(x.find(searchStringG, x.find(preStringG)))]
+                ##Replaces all commas with '&' symbols to create a well-formatted CSV
                 g = g.replace(',', '&')
+                ##Writes all objects to CSV comma delimited with line breaks
                 moving.write(z +", " + a + ", " + g + "\n")
-                print ('working')
+                ##Disabled Console logs for testing
+                # print ('working')
             except ValueError:
-                print ('something\'s fucky')
+                ##Disabled Console logs for testing
+                # print ('something\'s fucky')
+                ##Note: Data should be tested for completeness as well as correctl functionality
